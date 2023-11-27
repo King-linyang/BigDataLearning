5279d0eba5cdb5e12dcc8a52fd8d137479b5c0e4
wangly <2425942793@qq.com>
2023-11-28 00:19:43 +0800

    slf4j test

---
+++ Flink/src/test/java/com/ly/TestLog.java
@@ -0,0 +1,17 @@
+package com.ly;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestLog {
+    public static void main(String[] args) {
+        log.info("info");
+        log.debug("debug");
+        log.error("error");
+        log.warn("warn");
+        log.info("info");
+        log.debug("debug");
+        log.error("error");
+        log.warn("warn");
+    }
+}
